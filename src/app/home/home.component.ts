b78f746d604f19ab68ecff41fd2a5666f05f35a2
shahidshabir055 <shahidshabir055@gmail.com>
2021-11-24 13:56:05 +0530

    working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ export class HomeComponent implements OnInit {
   constructor(private httpClient: HttpClient) { }
   jobList: any = [];
   jobList1: any = [];
+  jobs: any =[];
   showCss: any = true;
   showFrontend: any = true;
   showJavascript: any = true;
@@ -17,16 +18,15 @@ export class HomeComponent implements OnInit {
     this.getData();
 }
  filterData(tag: any) {
-  this.getData();
+  this.jobList=this.jobs;
   this.jobList1 = this.jobList.filter((d:any) => d.languages.includes(tag) || d.role.includes(tag) );
-  console.log(this.jobList1);
   this.jobList =this.jobList1;
 
 }
 getData(){
   this.httpClient.get("assets/data.json").subscribe(data =>{
-    console.log(data);
     this.jobList = data;
+    this.jobs=data;
   })
 }
 
